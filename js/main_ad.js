df813fd0ecafc38219683e94892215d5ae4616a6
hwany <hwanyplaying@gmail.com>
2022-11-30 08:15:56 +0900

    first upload

---
+++ js/main_ad.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('.main_ad').slick({
+		dots: true,
+		autoplay:true,
+	})
+	
+	$('.main_ad').find('.slick-arrow').css('display','none')
+})
---
+++ js/navi.js
@@ -0,0 +1,7 @@
+$(function(){
+	$('ho1').hover(function(){
+		$('.sub_nav1').slideDown(500)
+	},function(){
+		$('.sub_nav1').slideUp(500)
+	})
+})
